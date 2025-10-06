1238b082808dae14106dea36f800917c2225361d
Tito Livaque <tclivaque@gmail.com>
2025-10-05 20:32:10 -0500

    mas cambios

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+function Menu() {
+  return (
+    <nav className="menu-lateral">
+      <h2>Menú</h2>
+      <ul>
+        <li>Incidencias</li>
+        <li>Informes</li>
+        <li>Configuración</li>
+        <li>Borrador</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu;
